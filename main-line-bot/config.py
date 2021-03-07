9bc205f388bfbc7645cdf1df288b955adcbce7ac
minagawa naoki <g020c1107@g.neec.ac.jp>
2021-03-07 21:11:47 +0900

    github

---
+++ main-line-bot/config.py
@@ -0,0 +1,12 @@
+# LINE
+Channel_access_token = ""
+Channel_secret = ""
+# Twitter
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+
+# googleアカウントにログイン
+neec_user_name = ""
+neec_pass = ""
